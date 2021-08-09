027fb8ac05827ee7aa8778bc4e95df2823bcdc36
Arthur0Rocha <ar_d_rocha@hotmail.com>
2021-08-09 18:59:53 -0300

    Updated set list refugio - TODO: Teto de vidro

--- new-lib/songs.py
+++ new-lib/songs.py
@@ -1,19 +1,41 @@
 refugio = [
-    (['PB011', 'CD016', 'CD017'], 'Mr. Crowley'),
-    (['PF011', 'CD029'], 'Man in the box'),
-    (['PF000'], 'Jeremy'),
-    (['PF000'], 'Machine head'),
-    (['PF001', 'PF003', 'PF004', 'PF000'], 'Don\'t speak'),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
-    # ([''], ''),
+    (['PB011', 'CD016', 'CD017'], 'Mr. Crowley'), # 01
+    (['PF011', 'CD029'], 'Man in the box'), # 02
+    (['PF000'], 'Jeremy'), # 03
+    (['PF000'], 'Machine head'), # 04
+    (['PF000', 'PF001', 'PF003', 'PF004'], 'Don\'t speak'), # 05
+    (['CD020', 'PD084'], 'It\'s my life'), # 06
+    (['PF001', 'CD001'], 'Toxicity'), # 07
+    (['PF008', 'PF000'], 'Love ain\'t no...'), # 08
+    (['PF003'], 'Highway to hell'), # 09
+    (['PF008', 'PF006', 'PF001', 'PF000'], 'Tears of the dragon'), # 10
+    (['PD057', 'PF000'], 'The evil that men do'), # 11
+    (['CD018'], 'The final countdown'), # 12
+    (['CD025'], 'Maniac'), # 13
+    (['PF000'], 'Breaking the law'), # 14
+    (['CD000', 'PF015'], 'I want out'), # 15
+    (['PF001', 'CD029', 'PF003'], 'Heaven and hell'), # 16
+    (['CD001PM', 'CD001PP'], 'Zombie'), # 17
+    (['CD026'], 'Take on me'), # 18
+    (['CD027'], 'I don\'t wanna miss...'), # 19
+    (['PF007'], 'Separate ways'), # 20
+    (['PF001', 'CD000'], 'Nothing else matters'), # 21
+    (['CD031'], 'Everybody wants to...'), # 22 
+    (['CD028', 'CD001PM', 'PA020'], 'Numb'), # 23 
+    (['PF007'], 'Jump'), # 24
+    (['PF001', 'CD001PM', 'PF000'], 'Wasting love'), # 25
+    (['CD022PP', 'CD022PM'], 'Perfect strangers'), # 26
+    (['CD001', 'PF001'], 'Bring me to life'), # 27
+    (['CD001PM', 'CD001PP'], 'My immortal'), # 28
+    (['CD021'], 'Going under'), # 29
+    (['PF008', 'PF001', 'CD029'], 'Teto de vidro'), # 30 TODO
+    (['PF001'], 'Admirável chip novo'), # 31
+    (['CD006', 'PF000', 'PF003'], 'Equalize'), # 32
+    (['PF000'], 'Pulsos'), # 33
+    (['CD029', 'PF001'], 'Everybody\'s fool'), # 34
+    (['CD019'], 'Enjoy the silence'), # 35
+    (['PF005'], 'Radio ga ga'), # 36
+    (['CD001PM', 'CD001PP'], 'Na sua estante') # 37
 ]
 
 ensaio = [
