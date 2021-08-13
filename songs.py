b13080975e4fa9657705795a562405bc6e64aaa5
Arthur0Rocha <ar_d_rocha@hotmail.com>
2021-08-13 10:05:27 -0300

    Changed ordering for official ordering for refugio

--- songs.py
+++ songs.py
@@ -1,4 +1,46 @@
 refugio = [
+    # Parte 1
+    (['CD025'], 'Maniac'), # 01
+    (['CD026'], 'Take on me'), # 02
+    (['PF001'], 'Admirável chip...'), # 03
+    (['CD028', 'CD001PM', 'PA020'], 'Numb'), # 04 
+    (['PF000'], 'Machine head'), # 05
+    (['PF000'], 'Jeremy'), # 06
+    (['CD006', 'PF001', 'CD029'], 'Teto de vidro'), # 07
+    (['PF011', 'CD029'], 'Man in the box'), # 08
+    (['PF000', 'PF001', 'PF003', 'PF004'], 'Don\'t speak'), # 09
+    (['CD001PM', 'CD001PP'], 'Zombie'), # 10
+    (['CD021'], 'Going under'), # 11
+    (['CD020', 'PD084'], 'It\'s my life'), # 12
+    (['PF000'], 'Pulsos'), # 13
+    (['CD031'], 'Everybody wants...'), # 14
+    (['PF003', 'PF000'], 'Highway to hell'), # 15
+    (['PF000'], 'Breaking the law'), # 16
+    (['CD006', 'PF000', 'PF003'], 'Equalize'), # 17
+    (['PF008', 'PF000'], 'Love ain\'t no...'), # 18
+    (['CD001', 'PF001'], 'Bring me to life'), # 19
+    (['PF001', 'CD001'], 'Toxicity'), # 20
+    # Parte 2    
+    (['CD018'], 'The final count...'), # 21
+    (['PF007'], 'Jump'), # 22
+    (['CD001PM', 'CD001PP'], 'My immortal'), # 23
+    (['PF007'], 'Separate ways'), # 24
+    (['CD022PP', 'CD022PM'], 'Perfect strangers'), # 25
+    (['PF001', 'CD000'], 'Nothing else m...'), # 26
+    (['CD029', 'PF001'], 'Everybody\'s fool'), # 27
+    (['CD019'], 'Enjoy the silence'), # 28
+    (['CD001PM', 'CD001PP'], 'Na sua estante') # 29
+    (['CD027'], 'I don\'t wanna m...'), # 30
+    (['PB011', 'CD016', 'CD017'], 'Mr. Crowley'), # 31
+    (['PF001', 'CD001PM', 'PF000'], 'Wasting love'), # 32
+    (['PF005'], 'Radio ga ga'), # 33
+    (['PF008', 'PF006', 'PF001', 'PF000'], 'Tears of the d...'), # 34
+    (['PD057', 'PF000'], 'The evil that...'), # 35
+    (['CD000', 'PF001', 'PF013'], 'I want out'), # 36
+    (['PF001', 'CD029'], 'Heaven and hell'), # 37
+]
+
+refugio_spotify = [
     (['PB011', 'CD016', 'CD017'], 'Mr. Crowley'), # 01
     (['PF011', 'CD029'], 'Man in the box'), # 02
     (['PF000'], 'Jeremy'), # 03
